3e2056b835653f7755524fe91fc5198f116975c9
Hanna Borodina <venera131916@gmail.com>
2023-11-07 23:42:57 +0200

    add updates, 8/10 tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+let loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let emailInput = loginForm.elements.email;
+  let passwordInput = loginForm.elements.password;
+
+  if (!emailInput.value || !passwordInput.value) {
+    alert("All fields must be filled in.");
+  } else {
+    let formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+let changeColor = document.querySelector(".change-color");
+let color = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColor.addEventListener("click", () => {
+  let getColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = getColor;
+  color.textContent = getColor;
+})
+
